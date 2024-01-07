4eb9839d691e6be8801e3162542d14545b7a9f07
Péter Tóth <toth00peter@gmail.com>
2024-01-07 19:03:11 +0100

    ui: added animation to music player + it's in BaseContentDiv now

--- src/layouts/sublayouts/MusicPlayer.tsx
+++ src/layouts/sublayouts/MusicPlayer.tsx
@@ -1,7 +1,8 @@
-import useSound from 'use-sound';
-import BaseContentDiv from '../../components/ui/BaseContentDiv';
+import useSound from "use-sound";
+import BaseContentDiv from "../../components/ui/BaseContentDiv";
 
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 const useAudio = (url: string) => {
   const [audio] = useState(new Audio(url));
@@ -10,34 +11,54 @@ const useAudio = (url: string) => {
   const toggle = () => setPlaying(!playing);
 
   useEffect(() => {
-      playing ? audio.play() : audio.pause();
-    },
-    [playing]
-  );
+    playing ? audio.play() : audio.pause();
+  }, [playing]);
 
   useEffect(() => {
-    audio.addEventListener('ended', () => setPlaying(false));
+    audio.addEventListener("ended", () => setPlaying(false));
     return () => {
-      audio.removeEventListener('ended', () => setPlaying(false));
+      audio.removeEventListener("ended", () => setPlaying(false));
     };
   }, []);
 
   return [playing, toggle];
 };
 
+const PlayerImg = styled.img<{ $playing?: boolean | (() => void) }>`
+  width: 200px;
+  animation: spin 5s linear infinite;
+  animation-play-state: ${({ $playing }) =>
+    $playing == true ? "running" : "paused"};
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
 function MusicPlayer() {
-  const [playing, toggle] = useAudio('/music/Jet Set Ratio.mp3');
+  const [playing, toggle] = useAudio("/music/Jet Set Ratio.mp3");
 
   return (
-    <BaseContentDiv style={{ alignItems: "center", textAlign: "center" }}>
-      <img 
-        style={{ width: "200px" }} 
-        src="/vecteezy_black-vinyl-disc-record-for-music-album-cover-design_9393830.png" 
+    <BaseContentDiv
+      style={{
+        textAlign: "center",
+        margin: "20px 0 20px 0",
+      }}
+    >
+      <PlayerImg
+        style={{ width: "200px" }}
+        src="/vecteezy_black-vinyl-disc-record-for-music-album-cover-design_9393830.png"
         alt="music disc"
         onClick={() => toggle()} // Add an onClick handler to the image
+        $playing={playing}
       />
     </BaseContentDiv>
-  )
+  );
 }
 
 export default MusicPlayer;
