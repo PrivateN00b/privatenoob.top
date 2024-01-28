b0a8b2aab61c23cfd4f71efe70c35c9a32c517bd
Péter Tóth <toth00peter@gmail.com>
2024-01-28 16:03:40 +0100

    cont: finished the Bejgli recipe

--- src/pages/blogs/Recipes/pages/Bejgli.tsx
+++ src/pages/blogs/Recipes/pages/Bejgli.tsx
@@ -17,7 +17,7 @@ export function Bejgli() {
         <StyledParagraph>Dough:</StyledParagraph>
         <ul>
           <li>
-            <p>1 kg flour</p>
+            <p>1 kg grated flour</p>
           </li>
           <li>
             <p>40 dkg butter or margarin</p>
@@ -32,6 +32,9 @@ export function Bejgli() {
             <p>10 dkg powdered sugar</p>
           </li>
           <li>
+            <p>2 dkg yeast</p>
+          </li>
+          <li>
             <p>1 coffespoon of salt</p>
           </li>
         </ul>
@@ -44,7 +47,10 @@ export function Bejgli() {
             <p>2-3 vanilla package</p>
           </li>
           <li>
-            <p>0.5 kg well and lightly minced walnut</p>
+            <p>
+              0.5 kg finely and crudely minced walnut (or just go finely mixed
+              all the way)
+            </p>
           </li>
           <li>
             <p>2 pieces of lemon skin</p>
@@ -53,7 +59,7 @@ export function Bejgli() {
             <p>1 piece of orange skin</p>
           </li>
           <li>
-            <p>1 tablespoon of cinnamon</p>
+            <p>1-2 tablespoon of cinnamon</p>
           </li>
           <li>
             <p>20-30 dkg sugar</p>
@@ -84,8 +90,136 @@ export function Bejgli() {
           <li>
             <p>100 g white chocolate</p>
           </li>
+          <li>
+            <p>1 piece of lemon skin</p>
+          </li>
+          <li>
+            <p>1 piece of orange skin</p>
+          </li>
         </ul>
         <CenteredH2>Steps</CenteredH2>
+        <ol>
+          <li>
+            <p>
+              Put the yeast and yolk into a room temperature milk and stir it a
+              bit
+            </p>
+          </li>
+          <li>
+            <p>
+              Put the powdered sugar and flour into a bowl and mix those a bit
+            </p>
+          </li>
+          <li>
+            <p>Include the butter to and do another mixing</p>
+          </li>
+          <li>
+            <p>
+              Pour the stirred milk mix into the the bowl and mix this until the
+              dough comes off your hands (This will take some time and arm
+              strength)
+            </p>
+          </li>
+          <li>
+            <p>
+              Create 4 equally sized dumplings and put these into the fridge
+              while all of them are wrapped in foils (Rest these doughs until
+              you're finished with the fillings)
+            </p>
+          </li>
+          <li>
+            <p>
+              Warm up both fillings's milk in a slow cook. If you're finished
+              then isolate half the milk for the walnut filling
+            </p>
+          </li>
+          <li>
+            <p>
+              COCONUT TIMEEEEE! While having the leftover milk in the cooking
+              pot, put the vanillin, coconut, butter (maybe the chocolate too if
+              you want?) then stir them
+            </p>
+          </li>
+          <li>
+            <p>
+              Include the salt and the flour into the filling and stir it until
+              they get creamy, but not runny. Add a tiny bit flour if it feels
+              too runny? Do some sample tasting if it needs any tweaks for ya.
+              If you're finished, cover the pot with a lid and let it coool
+              (Skip to walnut filling to save time)
+            </p>
+          </li>
+          <li>
+            <p>
+              After it has cooled down, grate 1 lemon and 1 orange skin into it.
+            </p>
+          </li>
+          <li>
+            <p>
+              WALNUT TIMEEEEE! Grab a bowl and put the walnut, cinnamon,
+              vanillin, orange and lemon skin and stir them a bit. It's not
+              necessarily a requirement, but you could have a finely and crudely
+              minced walnut.
+            </p>
+          </li>
+          <li>
+            <p>
+              Pour the warm milk into them and stir it. Don't let it be too dry
+              or too runny. Do some sample tasting if it needs any tweaks for
+              ya.
+            </p>
+          </li>
+          <li>
+            <p>
+              ASSEMBLING TIMEEEEE! Take out 1 dumpling dough, knead them out
+              into a rectangle / square shape if you can (I was only able to
+              make ellipses). The dough's width should be around 5-7 mm. (Using
+              a roller with warm water in the cylinder can help greatly. The
+              dough was really sticky for me ngl)
+            </p>
+          </li>
+          <li>
+            <p>
+              Spread half of the walnut or coconut filling on the dough as
+              evenly as you can with leaving 1 cm space on all sides. ONLY FOR
+              THE COCONUT: Smash the chocolate into tiny pieces and sprinkle
+              them on the fillings evenly. Do this for all the 3 remaining
+              dumplings
+            </p>
+          </li>
+          <li>
+            <p>
+              Press down the left and right edge of the dough with your fingers,
+              then fold those ONCE and press them down a bit again. This one
+              helps to create a better aesthetic look after the baking will
+              finished
+            </p>
+          </li>
+          <li>
+            <p>
+              Roll up the from down-to-top gently and lubricate the rolls with 1
+              egg yolk and put them into the freezer for 15 mins
+            </p>
+          </li>
+          <li>
+            <p>
+              Lubricate it with egg whites, wait a bit and just put them into
+              the oven for 200 Celsius in upper-under baking mode to 35 minutes
+              (or just watch until it looks tastyyy :3). If you have a modern
+              oven, you probably need to lower the heat to 160 C, I have
+              accidentally ruined this masterpiece
+            </p>
+          </li>
+          <li>
+            <p>
+              Don't eat yet hungry fella, just let it rest overnight with a
+              cloth over it.
+            </p>
+          </li>
+          <li>
+            <p>TASTE THE WONDER! ^^</p>
+          </li>
+        </ol>
         <CenteredH2>Sources</CenteredH2>
         <ul>
           <li>
