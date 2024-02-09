2f55d0dfd4394f0f94d83a39352f5d1c928c5fdb
Péter Tóth <toth00peter@gmail.com>
2024-02-09 23:17:53 +0100

    Added h1, h2 text gradient animations

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -10,7 +10,36 @@ const Container = styled.div`
 
   h1,
   h2 {
-    color: ${({ theme }) => theme.colors.primary};
+    /* color: ${({ theme }) => theme.colors.primary}; */
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+    background-image: linear-gradient(
+      45deg,
+      aqua,
+      magenta,
+      ${({ theme }) => theme.colors.primary}
+    );
+    animation: animate 5s linear infinite;
+    background-size: 400%;
+  }
+
+  @keyframes animate {
+    0% {
+      background-position: 0 0;
+    }
+    25% {
+      background-position: 100% 0;
+    }
+    50% {
+      background-position: 100% 100%;
+    }
+    75% {
+      background-position: 0 100%;
+    }
+    100% {
+      background-position: 0 0;
+    }
   }
 
   a:link {
