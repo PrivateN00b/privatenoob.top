16835026cd3611e5d289b73c629fd6b5bf3eadfc
Péter Tóth <toth00peter@gmail.com>
2024-02-10 12:30:12 +0100

    Transferred the h1, h2 style over to global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -22,6 +22,40 @@ const globalStyle = createGlobalStyle`
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   -webkit-text-size-adjust: 100%;
+
+    h1,
+  h2 {
+    /* color: ${({ theme }) => theme.colors.primary}; */
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+    background-image: linear-gradient(
+      45deg,
+      aqua,
+      magenta,
+      ${({ theme }) => theme.colors.primary}
+    );
+    animation: animate 5s linear infinite;
+    background-size: 400%;
+  }
+
+  @keyframes animate {
+    0% {
+      background-position: 0 0;
+    }
+    25% {
+      background-position: 100% 0;
+    }
+    50% {
+      background-position: 100% 100%;
+    }
+    75% {
+      background-position: 0 100%;
+    }
+    100% {
+      background-position: 0 0;
+    }
+  }
 }
 
 html {
