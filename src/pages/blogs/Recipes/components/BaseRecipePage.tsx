8f2c0a11b289f2382f28110cf35d632e4f2a2906
Péter Tóth <toth00peter@gmail.com>
2024-05-09 22:37:51 +0200

    Based Bejgli's page on BaseRecipePage

--- src/pages/blogs/Recipes/components/BaseRecipePage.tsx
+++ src/pages/blogs/Recipes/components/BaseRecipePage.tsx
@@ -27,14 +27,16 @@ type BaseRecipePageProps = {
   title: string;
   infos: Dict;
   ingredients: { [key: string]: string[] };
-  // steps: React.Component,
-  // sources: React.Component
+  steps: string[];
+  sources: { [key: string]: string }[];
 };
 
 export function BaseRecipePage({
   title,
   infos,
   ingredients,
+  steps,
+  sources,
 }: BaseRecipePageProps) {
   return (
     <Container>
@@ -53,185 +55,32 @@ export function BaseRecipePage({
             {Object.entries(ingredients).map(([key, value]) => (
               <>
                 <SubHeader>{key}</SubHeader>
-                {Object.entries(value).map(ingredient => <IngredientParagraph>{ingredient}</IngredientParagraph>)}
+                {Object.entries(value).map((ingredient) => (
+                  <IngredientParagraph>{ingredient}</IngredientParagraph>
+                ))}
               </>
             ))}
-            <SubHeader>Dough:</SubHeader>
-            <IngredientParagraph>1 kg grated flour</IngredientParagraph>
-            <IngredientParagraph>40 dkg butter or margarin</IngredientParagraph>
-            <IngredientParagraph>2 egg yolk</IngredientParagraph>
-            <IngredientParagraph>2,5 dl milk</IngredientParagraph>
-            <IngredientParagraph>10 dkg powdered sugar</IngredientParagraph>
-            <IngredientParagraph>2 dkg yeast</IngredientParagraph>
-            <IngredientParagraph>1 coffee spoon of salt</IngredientParagraph>
-            <SubHeader>Walnut filling for 2 rolls:</SubHeader>
-            <IngredientParagraph>3 dl milk</IngredientParagraph>
-            <IngredientParagraph>2-3 vanilla package</IngredientParagraph>
-            <IngredientParagraph>
-              0.5 kg finely and crudely minced walnut (or just go finely mixed
-              all the way)
-            </IngredientParagraph>
-            <IngredientParagraph>2 pieces of lemon skin</IngredientParagraph>
-            <IngredientParagraph>1 piece of orange skin</IngredientParagraph>
-            <IngredientParagraph>
-              1-2 tablespoon of cinnamon
-            </IngredientParagraph>
-            <IngredientParagraph>20-30 dkg sugar</IngredientParagraph>
-            <SubHeader>Coconut white chocolate filling for 2 rolls:</SubHeader>
-            <IngredientParagraph>3 dl milk</IngredientParagraph>
-            <IngredientParagraph>2 vanilla package</IngredientParagraph>
-            <IngredientParagraph>20 dkg grated coconut</IngredientParagraph>
-            <IngredientParagraph>2 dkg butter or margarin</IngredientParagraph>
-            <IngredientParagraph>1 pinch of salt</IngredientParagraph>
-            <IngredientParagraph>1 tablespoon of flour</IngredientParagraph>
-            <IngredientParagraph>100 g white chocolate</IngredientParagraph>
-            <IngredientParagraph>1 piece of lemon skin</IngredientParagraph>
-            <IngredientParagraph>1 piece of orange skin</IngredientParagraph>
           </LeftColumn>
           <RightColumn>
             <RightHeader>Steps</RightHeader>
             <StyledOL>
-              <li>
-                <p>
-                  Put the yeast and yolk into a room temperature milk and stir
-                  it a bit
-                </p>
-              </li>
-              <li>
-                <p>
-                  Put the powdered sugar and flour into a bowl and mix those a
-                  bit
-                </p>
-              </li>
-              <li>
-                <p>Include the butter to and do another mixing</p>
-              </li>
-              <li>
-                <p>
-                  Pour the stirred milk mix into the the bowl and mix this until
-                  the dough comes off your hands (This will take some time and
-                  arm strength)
-                </p>
-              </li>
-              <li>
-                <p>
-                  Create 4 equally sized dumplings and put these into the fridge
-                  while all of them are wrapped in foils (Rest these doughs
-                  until you're finished with the fillings)
-                </p>
-              </li>
-              <li>
-                <p>
-                  Warm up both fillings's milk in a slow cook. If you're
-                  finished then isolate half the milk for the walnut filling
-                </p>
-              </li>
-              <li>
-                <p>
-                  COCONUT TIMEEEEE! While having the leftover milk in the
-                  cooking pot, put the vanillin, coconut, butter (maybe the
-                  chocolate too if you want?) then stir them
-                </p>
-              </li>
-              <li>
-                <p>
-                  Include the salt and the flour into the filling and stir it
-                  until they get creamy, but not runny. Add a tiny bit flour if
-                  it feels too runny? Do some sample tasting if it needs any
-                  tweaks for ya. If you're finished, cover the pot with a lid
-                  and let it coool (Skip to walnut filling to save time)
-                </p>
-              </li>
-              <li>
-                <p>
-                  After it has cooled down, grate 1 lemon and 1 orange skin into
-                  it.
-                </p>
-              </li>
-              <li>
-                <p>
-                  WALNUT TIMEEEEE! Grab a bowl and put the walnut, cinnamon,
-                  vanillin, orange and lemon skin and stir them a bit. It's not
-                  necessarily a requirement, but you could have a finely and
-                  crudely minced walnut.
-                </p>
-              </li>
-              <li>
-                <p>
-                  Pour the warm milk into them and stir it. Don't let it be too
-                  dry or too runny. Do some sample tasting if it needs any
-                  tweaks for ya.
-                </p>
-              </li>
-              <li>
-                <p>
-                  ASSEMBLING TIMEEEEE! Take out 1 dumpling dough, knead them out
-                  into a rectangle / square shape if you can (I was only able to
-                  make ellipses). The dough's width should be around 5-7 mm.
-                  (Using a roller with warm water in the cylinder can help
-                  greatly. The dough was really sticky for me ngl)
-                </p>
-              </li>
-              <li>
-                <p>
-                  Spread half of the walnut or coconut filling on the dough as
-                  evenly as you can with leaving 1 cm space on all sides. ONLY
-                  FOR THE COCONUT: Smash the chocolate into tiny pieces and
-                  sprinkle them on the fillings evenly. Do this for all the 3
-                  remaining dumplings
-                </p>
-              </li>
-              <li>
-                <p>
-                  Press down the left and right edge of the dough with your
-                  fingers, then fold those ONCE and press them down a bit again.
-                  This one helps to create a better aesthetic look after the
-                  baking will finished
-                </p>
-              </li>
-              <li>
-                <p>
-                  Roll up the from down-to-top gently and lubricate the rolls
-                  with 1 egg yolk and put them into the freezer for 15 mins
-                </p>
-              </li>
-              <li>
-                <p>
-                  Lubricate it with egg whites, wait a bit and just put them
-                  into the oven for 200 Celsius in upper-under baking mode to 35
-                  minutes (or just watch until it looks tastyyy :3). If you have
-                  a modern oven, you probably need to lower the heat to 160 C, I
-                  have accidentally ruined this masterpiece
-                </p>
-              </li>
-              <li>
-                <p>
-                  Don't eat yet hungry fella, just let it rest overnight with a
-                  cloth over it.
-                </p>
-              </li>
-              <li>
-                <p>TASTE THE WONDER! ^^</p>
-              </li>
+              {steps.map((step) => (
+                <li>
+                  <p>{step}</p>
+                </li>
+              ))}
             </StyledOL>
           </RightColumn>
         </Row>
 
         <CenteredH2>Sources</CenteredH2>
         <StyledUL style={{ marginBottom: "20px" }}>
-          <li>
-            <a href="https://youtu.be/rTyQWQYn318?si=qoFNhAWbbub-E9Sk">
-              Bejgli Beigli Diós Mákos @Szoky konyhája.
-            </a>{" "}
-            Retrieved 2024.01.27
-          </li>
-          <li>
-            {" "}
-            <a href="https://colore.hu/egeszseg/recept/kokuszos-feher-csokis-bejgli-ami-nem-reped-videoval/">
-              Kókuszos-fehércsokis bejgli, ami nem reped! (Videóval!).
-            </a>{" "}
-            Retrieved 2024.01.27
-          </li>
+          {sources.map((source) => (
+            <li>
+              <a href={source["url"]}>{source["title"]}</a> Retrieved{" "}
+              {source["retrieved"]}
+            </li>
+          ))}
         </StyledUL>
       </BaseContentDiv>
     </Container>
