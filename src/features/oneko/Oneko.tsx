9bb54ffdcbf6fef44a0d289a1f4e184f2d3f03d6
PrivateN00b <petoth@proton.me>
2024-12-22 22:24:20 +0100

    Added sprites with basic directions

--- src/features/oneko/Oneko.tsx
+++ src/features/oneko/Oneko.tsx
@@ -1,8 +1,73 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useMouseCoords } from "./useMouseCoords";
 import styled from "styled-components";
+import spriteSheet from "/oneko.png";
 
-const Sprite = styled.img<{ $top: number; $left: number }>`
+// This is how we can "index" the spritesheet
+const spriteSets: SpriteSets = {
+  idle: [[-3, -3]],
+  alert: [[-7, -3]],
+  scratchSelf: [
+    [-5, 0],
+    [-6, 0],
+    [-7, 0],
+  ],
+  scratchWallN: [
+    [0, 0],
+    [0, -1],
+  ],
+  scratchWallS: [
+    [-7, -1],
+    [-6, -2],
+  ],
+  scratchWallE: [
+    [-2, -2],
+    [-2, -3],
+  ],
+  scratchWallW: [
+    [-4, 0],
+    [-4, -1],
+  ],
+  tired: [[-3, -2]],
+  sleeping: [
+    [-2, 0],
+    [-2, -1],
+  ],
+  N: [
+    [-1, -2],
+    [-1, -3],
+  ],
+  NE: [
+    [0, -2],
+    [0, -3],
+  ],
+  E: [
+    [-3, 0],
+    [-3, -1],
+  ],
+  SE: [
+    [-5, -1],
+    [-5, -2],
+  ],
+  S: [
+    [-6, -3],
+    [-7, -2],
+  ],
+  SW: [
+    [-5, -3],
+    [-6, -1],
+  ],
+  W: [
+    [-4, -2],
+    [-4, -3],
+  ],
+  NW: [
+    [-1, 0],
+    [-1, -1],
+  ],
+};
+
+const Sprite = styled.div<{ $top: number; $left: number }>`
   position: absolute;
   z-index: 1000;
   top: ${({ $top }) => $top}px;
@@ -12,10 +77,21 @@ const Sprite = styled.img<{ $top: number; $left: number }>`
   height: 32px;
 `;
 
+type SpriteSets = {
+  [key: string]: number[][];
+}
+
+const setSprite = (img: HTMLImageElement, name: string, frame: number) => {
+  const sprite: number[] = spriteSets[name][frame % spriteSets[name].length];
+  img.style.backgroundImage = `url(${spriteSheet})`;
+  img.style.backgroundPosition = `${sprite[0] * 32}px ${sprite[1] * 32}px`; // Show the appropiate sprite, by cutting the image
+}
+
 export default function Oneko() {
   const spriteRef = useRef<HTMLImageElement | null>(null);
+  const animFrameId = useRef<number>()
   const coords = useMouseCoords();
-  const [position, setPosition] = useState({
+  const positionRef = useRef({ // Starting idx of the Sprite, but it's initial position is separately defined
     top: window.innerHeight,
     left: 0.8 * window.innerWidth,
   });
@@ -23,41 +99,53 @@ export default function Oneko() {
   // The Sprite's position changes on each frame, going towards the current mouse position
   // The frame frequency matches the monitor's refresh rate (ex: function runs 60 times on a 60 Hz monitor)
   useEffect(() => {
-    let animFrameId: number;
-
     function frame() {
-      setPosition((currentPos) => {
-        const speed = 1;
-        const dx = coords.x - currentPos.left;
-        const dy = coords.y - currentPos.top;
-        const distance = Math.sqrt(dx * dx + dy * dy); // Distance between 2 objects.
-        
-        // Stop the sprite if it's close enough to the cursor
-        if (Math.abs(dx) <= 4 && Math.abs(dy) <= 4) return currentPos;
-        
-        return {
-          top: currentPos.top + speed * (dy / distance),
-          left: currentPos.left + speed * (dx / distance),
-        }
-      });
+      const speed = 1;
+      const dx = coords.x - positionRef.current.left;
+      const dy = coords.y - positionRef.current.top;
+      const distance = Math.sqrt(dx * dx + dy * dy); // Distance between 2 objects.
+    
+      // Selecting sprite based on direction
+      if (spriteRef.current) {
+        let direction = "";
+        if (Math.abs(dy / distance) > 0.5) direction += dy > 0 ? "S" : "N";
+        if (Math.abs(dx / distance) > 0.5) direction += dx > 0 ? "E" : "W";
+        setSprite(spriteRef.current, direction, 0)
+      } 
+
+      // Stop the sprite if it's close enough to the cursor
+      if (Math.abs(dx) <= 4 && Math.abs(dy) <= 4) return positionRef.current;
       
-      animFrameId = requestAnimationFrame(frame)
+      // Update positions
+      positionRef.current = {
+        top: positionRef.current.top + speed * (dy / distance),
+        left: positionRef.current.left + speed * (dx / distance),
+      }
+      if (spriteRef.current) {
+        spriteRef.current.style.top = `${positionRef.current.top}px`
+        spriteRef.current.style.left = `${positionRef.current.left}px`
+      } 
+      
+      animFrameId.current = requestAnimationFrame(frame)
     }
 
-    animFrameId = requestAnimationFrame(frame)
+    animFrameId.current = requestAnimationFrame(frame)
 
-
-    return () => cancelAnimationFrame(animFrameId)
+    // Cleanup
+    return () => {
+      if (animFrameId.current) {
+        cancelAnimationFrame(animFrameId.current);
+        animFrameId.current = undefined;
+      }
+    };
   }, [coords])
-
 
   return (
     <Sprite
+      id="oneko"
       ref={spriteRef}
-      src="/emoji/ok.png"
-      $top={position.top}
-      $left={position.left}
-      alt="Following sprite"
+      $top={window.innerHeight}
+      $left={0.8 * window.innerWidth}
     />
   );
 }
