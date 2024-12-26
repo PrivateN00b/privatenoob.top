d88a4d61091818f69d7443102ac261e548dac76c
PrivateN00b <petoth@proton.me>
2024-12-26 21:17:48 +0100

    Extended DeezerEmbed's height to 400

--- src/features/deezerEmbed/DeezerEmbed.tsx
+++ src/features/deezerEmbed/DeezerEmbed.tsx
@@ -15,7 +15,7 @@ export default function DeezerEmbed({ isLastBottom, isLastLeft }: BaseProps) {
         <DeezerIFrame title="deezer-widget" 
                     src="https://widget.deezer.com/widget/auto/playlist/13306681843" 
                     width="100%" 
-                    height="300" 
+                    height="400" 
                     allowTransparency={true}
                     allow="encrypted-media; clipboard-write"></DeezerIFrame>
     </BaseContentDiv>
