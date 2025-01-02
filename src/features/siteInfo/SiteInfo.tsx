816b79b069e0d100490470e87ad249ede38ec6fd
PrivateN00b <petoth@proton.me>
2025-01-02 14:06:30 +0100

    Added code from react-websitecarbon-badge package to modify later

--- src/features/siteInfo/SiteInfo.tsx
+++ src/features/siteInfo/SiteInfo.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import BaseContentDiv from "../../components/div/BaseContentDiv";
 import { BaseProps } from "../../utils/interfaces";
 import { FlexDivWithPadding } from "../../components/div/FlexDivs";
-import { useEffect, useRef } from "react";
+import WebsiteCarbonBadge from "./WebsiteCarbonBadge/WebsiteCarbonBadge";
 
 const SiteInfoStyle = styled(BaseContentDiv)`
     padding-top: 20px;
@@ -13,27 +13,12 @@ const ClickableImg = styled.img`
 `;
 
 export default function SiteInfo({ isLastBottom, isLastLeft }: BaseProps) {
-    const carbonBadgeRef = useRef(null);
-
-    useEffect(() => {
-        if (carbonBadgeRef.current) {
-            const script = document.createElement('script');
-            script.src = "https://unpkg.com/website-carbon-badges@1.1.3/b.min.js";
-            script.async = true;
-            document.body.appendChild(script);
-
-            return () => {
-                document.body.removeChild(script);
-            };
-        }
-    }, []);
-
     return (
         <SiteInfoStyle isLastBottom={isLastBottom} isLastLeft={isLastLeft}>
             <FlexDivWithPadding>
                 <ClickableImg src="/infos/Censor_PGb.gif" onClick={() => window.open("http://www.mabsland.com/Adoption.html")}/>
             </FlexDivWithPadding>
-            <div id="wcb" className="carbonbadge wcb-d" ref={carbonBadgeRef}></div>
+            <WebsiteCarbonBadge url="www.privatenoob.top" />
         </SiteInfoStyle>
     );
 }
