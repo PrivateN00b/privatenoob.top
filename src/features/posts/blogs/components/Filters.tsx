307fbd54199dba49644fb51fdfca93fabc7b17e2
PrivateN00b <petoth@proton.me>
2025-01-04 12:37:48 +0100

    Added an initial Filters component for Blogs

---
+++ src/features/posts/blogs/components/Filters.tsx
@@ -0,0 +1,23 @@
+import BaseContentDiv from "../../../../components/div/BaseContentDiv";
+import CheckBox from "../../../../components/checkbox/Checkbox";
+import { BaseProps } from "../../../../utils/interfaces";
+
+interface FiltersProps extends BaseProps {
+  onFilterChange: (
+    name: string,
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => void;
+}
+
+export default function Filters({
+  isLastBottom = false,
+  onFilterChange,
+}: FiltersProps) {
+  return (
+    <BaseContentDiv $isLastBottom={isLastBottom} style={{ padding: "20px 0 0 0" }}>
+      <CheckBox name="Coding" onCheckBoxChange={onFilterChange} />
+      <CheckBox name="Food" onCheckBoxChange={onFilterChange} />
+      <CheckBox name="Gaming" onCheckBoxChange={onFilterChange} />
+    </BaseContentDiv>
+  );
+}
