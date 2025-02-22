b59aa0097091da6a1b14fe1a674663bca114121c
PrivateN00b <petoth@proton.me>
2025-02-22 19:45:37 +0100

    Added a vertical segment as well, and fixec the angle not being 45 degrees

--- src/features/posts/recipes/components/segmentDisplay.ts
+++ src/features/posts/recipes/components/segmentDisplay.ts
@@ -5,23 +5,46 @@ export default function createSegmentDisplay() {
     const canvas: HTMLCanvasElement = document.getElementById("segmentDisplayCanvas") as HTMLCanvasElement;
     ctx = canvas.getContext("2d");
 
-    // Segment A
-    createHexagon(200, 10, 20, 6)
+    // Display segments
+    createHexagon(200, 10, 20, 6, "horizontal")  // Segment A
+    createHexagon(200, 10, 20, 6, "vertical")  // Segment B
 }
 
-const createHexagon = (x: number, y: number, w: number, h: number) => {
+const createHexagon = (x: number, y: number, w: number, h: number, dir: string) => {
     if (ctx != null) {
+        ctx.fillStyle = "red"
         // Every lineTo call is called from the most recently called moveTo function
         // This means that the 2nd lineTo has to contain the movements of 1st lineTo as well etc. etc.
-        ctx.fillStyle = "red"
-        ctx.moveTo(x, y)
-        ctx.lineTo(x + 0.25 * w, y - 0.5 * h)
-        ctx.lineTo(x + 1.25 * w, y - 0.5 * h)  // X: x + 0.25 * w + w | Y: y - 0.5 * h
-        ctx.lineTo(x + 1.5 * w, y)  // X: x + 0.25 * w + w + 0.25 * w | Y: y - 0.5 * h + 0.5 * h
+        if (dir === "horizontal") {
+            // Get the width length if we want to have a 45 angle segment
+            const w45: number = calculateSegmentWidth(w, h)
+            console.log(w45)
 
-        ctx.lineTo(x + 1.25 * w, y + 0.5 * h)
-        ctx.lineTo(x + 0.25 * w, y + 0.5 * h)
-        ctx.lineTo(x, y)
-        ctx.fill()
+            ctx.moveTo(x, y)
+            ctx.lineTo(x + 0.5 * h, y - 0.5 * h)
+            ctx.lineTo(x + 0.5 * h + w, y - 0.5 * h)
+            ctx.lineTo(x + w + h, y)
+    
+            ctx.lineTo(x + 0.5 * h + w, y + 0.5 * h)
+            ctx.lineTo(x + 0.5 * h, y + 0.5 * h)
+            ctx.lineTo(x, y)
+            ctx.fill()
+        }
+        else if (dir === "vertical") {
+            ctx.moveTo(x, y)
+            ctx.lineTo(x + 0.5 * h, y + 0.5 * h)
+            ctx.lineTo(x + 0.5 * h, y + w + 0.5 * h)
+            ctx.lineTo(x, y + w + h)
+            
+            ctx.lineTo(x - 0.5 * h, y + w + 0.5 * h)
+            ctx.lineTo(x - 0.5 * h, y + 0.5 * h)
+            ctx.lineTo(x, y)
+
+            ctx.stroke()
+        }
     }
+}
+
+const calculateSegmentWidth = (w: number, h: number) => {
+    return Math.acos(1)
 }
