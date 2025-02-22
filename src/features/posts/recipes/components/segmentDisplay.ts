25eff0eac5b264710508ca87f301c088eab90fdf
PrivateN00b <petoth@proton.me>
2025-02-22 23:11:30 +0100

    Added a dot

--- src/features/posts/recipes/components/segmentDisplay.ts
+++ src/features/posts/recipes/components/segmentDisplay.ts
@@ -7,12 +7,12 @@ export default function createSegmentDisplay() {
     const canvasHeight: number = canvas.height;
     const canvasWidth: number = canvas.width;
     const gap: number = 1;
-    const gapBetweenNums: number = 10;
+    const gapBetweenNums: number = 12;
     const height: number = 6;
     const width: number = 18;
     const hexWidth: number = (width + height);
 
-    // Display segments
+    // Display tens
     createHexagon(0.5 * canvasWidth - hexWidth - gapBetweenNums, 0.5 * canvasHeight - hexWidth - 2 * gap, width, height, "horizontal")  // Segment A
     createHexagon(0.5 * canvasWidth - gapBetweenNums + gap, 0.5 * canvasHeight - hexWidth - gap, width, height, "vertical")  // Segment B
     createHexagon(0.5 * canvasWidth - gapBetweenNums + gap, 0.5 * canvasHeight + gap, width, height, "vertical")  // Segment C
@@ -20,7 +20,11 @@ export default function createSegmentDisplay() {
     createHexagon(0.5 * canvasWidth - hexWidth - gapBetweenNums - gap, 0.5 * canvasHeight + gap, width, height, "vertical")  // Segment E
     createHexagon(0.5 * canvasWidth - hexWidth - gapBetweenNums - gap, 0.5 * canvasHeight - hexWidth - gap, width, height, "vertical")  // Segment F
     createHexagon(0.5 * canvasWidth - hexWidth - gapBetweenNums, 0.5 * canvasHeight, width, height, "horizontal")  // Segment G
+   
+    // Display dot
+    createDot(0.5 * canvasWidth, 0.5 * canvasHeight + hexWidth + 2 * gap, 4)
 
+    // Display ones
     createHexagon(0.5 * canvasWidth + gapBetweenNums, 0.5 * canvasHeight - hexWidth - 2 * gap, width, height, "horizontal")  // Segment A
     createHexagon(0.5 * canvasWidth + hexWidth + gapBetweenNums + gap, 0.5 * canvasHeight - hexWidth - gap, width, height, "vertical")  // Segment B
     createHexagon(0.5 * canvasWidth + hexWidth + gapBetweenNums + gap, 0.5 * canvasHeight + gap, width, height, "vertical")  // Segment C
@@ -36,10 +40,6 @@ const createHexagon = (x: number, y: number, w: number, h: number, dir: string) 
         // Every lineTo call is called from the most recently called moveTo function
         // This means that the 2nd lineTo has to contain the movements of 1st lineTo as well etc. etc.
         if (dir === "horizontal") {
-            // Get the width length if we want to have a 45 angle segment
-            const w45: number = calculateSegmentWidth(w, h)
-            console.log(w45)
-
             ctx.moveTo(x, y)
             ctx.lineTo(x + 0.5 * h, y - 0.5 * h)
             ctx.lineTo(x + 0.5 * h + w, y - 0.5 * h)
@@ -65,6 +65,14 @@ const createHexagon = (x: number, y: number, w: number, h: number, dir: string) 
     }
 }
 
-const calculateSegmentWidth = (w: number, h: number) => {
-    return Math.acos(1)
+const createDot = (x: number, y: number, r: number) => {
+    if (ctx != null) {
+        ctx.fillStyle = "#fd5800"
+        // Every lineTo call is called from the most recently called moveTo function
+        // This means that the 2nd lineTo has to contain the movements of 1st lineTo as well etc. etc.
+        ctx.moveTo(x, y)
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, 2 * Math.PI);
+        ctx.fill()
+    }
 }
