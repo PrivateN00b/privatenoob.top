e2176e025aaae8801fd878ccca529a5bd4f70e79
PrivateN00b <petoth@proton.me>
2025-05-11 16:23:46 +0200

    Refactored DeezerEmbed with stylex and lazy loading

--- src/features/deezerEmbed/DeezerEmbed.tsx
+++ src/features/deezerEmbed/DeezerEmbed.tsx
@@ -1,24 +1,26 @@
-import styled from "styled-components"
 import BaseContentDiv from "../../components/div/BaseContentDiv"
 import { CenteredH2 } from "../../components/text/CenteredHeaders";
 import { BaseContentDivProps } from "../../utils/interfaces"
+import * as stylex from '@stylexjs/stylex';
 
-const DeezerIFrame = styled.iframe`
-  border: 0;
-  border-radius: 3% 3% 5% 5%;
-  margin-bottom: -5px;
-`
+const styles = stylex.create({
+  base: {
+    border: "0",
+    borderRadius: "3% 3% 5% 5%",
+    marginBottom: "-5px"
+  }
+})
 
 export default function DeezerEmbed({ $isLastBottom, $isLastLeft }: BaseContentDivProps) {
     return  <BaseContentDiv $isLastBottom={$isLastBottom} $isLastLeft={$isLastLeft}>
         <CenteredH2>🎶 Fav J-songs 🎶</CenteredH2>
-        <DeezerIFrame title="deezer-widget" 
+        <iframe {...stylex.props(styles.base)}
+                    title="deezer-widget" 
                     src="https://widget.deezer.com/widget/auto/playlist/13306681843" 
                     width="100%" 
                     height="400" 
                     allowTransparency={true}
-                    allow="encrypted-media; clipboard-write"></DeezerIFrame>
+                    allow="encrypted-media; clipboard-write"
+                    loading="lazy"></iframe>
     </BaseContentDiv>
-    
-    
 }
