b2a379b5b81d99b5b6cde1669ebad573df444ba0
PrivateN00b <petoth@proton.me>
2025-05-25 13:39:48 +0200

    Reworked Buttons with stylex

--- src/features/core/home/components/Buttons.tsx
+++ src/features/core/home/components/Buttons.tsx
@@ -1,73 +1,76 @@
-import styled from "styled-components";
+import * as stylex from '@stylexjs/stylex';
 
-const InnerButtons = styled.div`
-    display: flex;
-    animation: scroll 10s linear infinite;
-    gap: var(--gap);
+const gap: string = "10px"
 
-    @keyframes scroll {
-        to {
-            /* We need to remove the gap amount for seeing a stable animation reset */
-            transform: translateX(calc(-100% - var(--gap)));
+const scroll = stylex.keyframes({
+    /* We need to remove the gap amount for seeing a stable animation reset */
+    to: { transform: `translateX(calc(-100% - ${gap}))` },
+});
+
+const styles = stylex.create({
+    innerButtons: {
+        display: "flex",
+        animationName: scroll,
+        animationDuration: '10s',
+        animationTimingFunction: 'linear',
+        animationIterationCount: 'infinite',
+        gap: gap,
+    },
+    buttonsDiv: {
+        width: "100%", /* Ensure the container takes the full width */
+        display: "flex",
+        overflow: "hidden", /* Hide overflow */
+        flexShrink: "0",
+        marginBottom: "20px",
+        gap: gap,
+        height: "31px",
+        
+        ":hover > *": {
+            animationPlayState: "paused"
         }
+    },
+    button: {
+        width: "88px",
+        height: "31px"
     }
-`;
-
-const ButtonsDiv = styled.div`
-    --gap: 10px;
-    width: 100%; /* Ensure the container takes the full width */
-    display: flex;
-    overflow: hidden; /* Hide overflow */
-    flex-shrink: 0;
-    margin-bottom: 20px;
-    gap: var(--gap);
-
-    &:hover ${InnerButtons} {
-        animation-play-state: paused;
-    }
-`;
-
-const Button = styled.img`
-    width: 88px;
-    height: 31px;
-`
+});
 
 export default function Buttons() {
     return (
-        <ButtonsDiv>
-            <InnerButtons>
-                <Button src="infos/buttons/tested-on-firefox.webp" alt="" loading="lazy"/>
-                <Button src="infos/buttons/animegay.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/kde-now.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/endeavouros.jpg" alt="" loading="lazy"/>
-                <Button src="infos/buttons/linux.webp" alt="" loading="lazy"/>
-                <Button src="infos/buttons/blockads.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/miku3.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/neo-fedi.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/neocities_button.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/old-net.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/piracy.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/seed.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/steam.gif" alt="" loading="lazy"/>
-            </InnerButtons>
+        <div {...stylex.props(styles.buttonsDiv)}>
+            <div {...stylex.props(styles.innerButtons)}>
+                <img {...stylex.props(styles.button)} src="infos/buttons/tested-on-firefox.webp" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/animegay.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/kde-now.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/endeavouros.jpg" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/linux.webp" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/blockads.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/miku3.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/neo-fedi.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/neocities_button.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/old-net.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/piracy.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/seed.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/steam.gif" alt="" loading="lazy"/>
+            </div>
 
             {/* Content is duplicated to make the infinite scroll feeling work
             I don't want to use JS to generate the doubled content */}
-            <InnerButtons>
-                <Button src="infos/buttons/tested-on-firefox.webp" alt="" loading="lazy"/>
-                <Button src="infos/buttons/animegay.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/kde-now.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/endeavouros.jpg" alt="" loading="lazy"/>
-                <Button src="infos/buttons/linux.webp" alt="" loading="lazy"/>
-                <Button src="infos/buttons/blockads.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/miku3.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/neo-fedi.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/neocities_button.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/old-net.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/piracy.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/seed.gif" alt="" loading="lazy"/>
-                <Button src="infos/buttons/steam.gif" alt="" loading="lazy"/>
-            </InnerButtons>
-        </ButtonsDiv>
+            <div {...stylex.props(styles.innerButtons)}>
+                <img {...stylex.props(styles.button)} src="infos/buttons/tested-on-firefox.webp" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/animegay.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/kde-now.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/endeavouros.jpg" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/linux.webp" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/blockads.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/miku3.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/neo-fedi.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/neocities_button.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/old-net.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/piracy.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/seed.gif" alt="" loading="lazy"/>
+                <img {...stylex.props(styles.button)} src="infos/buttons/steam.gif" alt="" loading="lazy"/>
+            </div>
+        </div>
     );
 }
