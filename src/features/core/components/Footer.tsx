93bd6121429134e77afbcf83ab330ab3935e9ca0
PrivateN00b <petoth@proton.me>
2025-06-09 23:01:57 +0200

    Include latest update from public repo data and 'copyright' is dynamically updated now

--- src/features/core/components/Footer.tsx
+++ src/features/core/components/Footer.tsx
@@ -2,6 +2,7 @@ import { colors, colorsA } from "../../../styles/tokens.stylex";
 import { CenteredH2 } from "../../../components/text/CenteredHeaders";
 import * as stylex from '@stylexjs/stylex';
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const styles = stylex.create({
   footer: {
@@ -35,16 +36,31 @@ const styles = stylex.create({
 })
 
 function Footer() {
+  const currentYear: number = (new Date()).getFullYear();  // Get this year
+  const [lastUpdate, setLastUpdate] = useState("2025")
+
+  // Check the freshest commit on the public repo (https://github.com/PrivateN00b/privatenoob.top)
+  useEffect(() => {
+    const url = "https://api.github.com/repos/PrivateN00b/privatenoob.top/commits?per_page=1";
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        const date: Date = new Date(res[0]["commit"]["author"]["date"]);
+        const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: '2-digit' };  // Use a custom format to have a more readable Date text
+        const customDate: string = date.toLocaleDateString("en-US", options);
+        setLastUpdate(customDate)
+      })
+  }, [])
+
   return (
     <footer {...stylex.props(styles.footer)}>
       <div style={{ width: "30%" }}>
         <CenteredH2>Infos</CenteredH2>
-        <p>Last Update: 2024</p>
-        <p>Total Views: UNDER CONSTRUCTION </p>
+        <p>Last Update: {lastUpdate}</p>
       </div>
       <div style={{ width: "40%" }}>
         <CenteredH2>Boring stuff</CenteredH2>
-        <p>© PrivateNoob 2023-2024. Do whatever the hell you want with it.</p>
+        <p>© PrivateNoob 2023-{currentYear}. Do whatever the hell you want with it.</p>
       </div>
       <div style={{ width: "30%", display: "flex", flexDirection: "column" }}>
         <CenteredH2>Links</CenteredH2>
