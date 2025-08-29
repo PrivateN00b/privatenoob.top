0c1e69856b6d982aaf82c9e5590cf131b5da601b
PrivateN00b <petoth@proton.me>
2025-08-29 20:38:53 +0200

    Added vike-react-styled-components

--- src/+config.ts
+++ src/+config.ts
@@ -1,7 +1,8 @@
 import type { Config } from 'vike/types'
 import vikeReact from 'vike-react/config'
+import vikeReactStyledComponents from "vike-react-styled-components/config"
 
 export default {
   stream: true,
-  extends: [vikeReact]
+  extends: [vikeReact, vikeReactStyledComponents]
 } satisfies Config
