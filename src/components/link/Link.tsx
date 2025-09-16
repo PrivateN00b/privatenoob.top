789ab2e2df1adcfa28743a8820171879f9a65269
PrivateN00b <petoth@proton.me>
2025-09-16 20:20:58 +0200

    Removed some unnecessary lines

--- src/components/link/Link.tsx
+++ src/components/link/Link.tsx
@@ -1,10 +1,6 @@
 import * as stylex from '@stylexjs/stylex';
 
 export function Link({ style, href, children }: { style: Readonly<{}>[], href: string; children: React.ReactNode }) {
-  // const pageContext = usePageContext();
-  // const { urlPathname } = pageContext;
-  // const isActive = href === "/" ? urlPathname === href : urlPathname.startsWith(href);
-
   return (
     <a {...stylex.props(style)} href={href}>
       {children}
